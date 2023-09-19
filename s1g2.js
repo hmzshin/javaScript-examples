f276661b0bb9c71d5b2483756b17ab851a726112
hmzshin <hamzasahin@outlook.com>
2023-09-19 12:36:21 +0300

    güncelleme

--- s1g2.js
+++ s1g2.js
@@ -1,41 +1,35 @@
-
 // 1a
 
-let surucuYası=15;
-if (surucuYası> 18) {
+let surucuYası = 15;
+if (surucuYası > 18) {
   console.log(true);
-}else{
+} else {
   console.log(false);
 }
 
 //1b
 
 let birinciDeger = 22;
-let ikinciDeger=35;
+let ikinciDeger = 35;
 
-if (birinciDeger>=ikinciDeger) {
-  birinciDeger=67;  
+if (birinciDeger >= ikinciDeger) {
+  birinciDeger = 67;
 }
 
-console.log(birinciDeger)
+console.log(birinciDeger);
 
 //1c
 
-let sayı="1999";
+let sayı = "1999";
 let yeni_sayı = Number(sayı);
 console.log(typeof yeni_sayı);
-
-
 
 //1d
 
 function carpma(num1, num2) {
-
   return num1 * num2;
-
 }
 console.log(carpma(7, 4));
-
 
 //2
 
@@ -45,53 +39,48 @@ function kopeginYasi(yıl) {
 }
 console.log(`Köpeğin yaşı = ${kopeginYasi(3)}`);
 
-
 //3
 
 function oyun(oyuncu, bilgisayar) {
-
-  if (oyuncu == 'Taş' && bilgisayar == 'Taş') {
+  if (oyuncu == "Taş" && bilgisayar == "Taş") {
     console.log("Beraberlik");
-  } else if (oyuncu == 'Kağıt' && bilgisayar == 'Kağıt') {
+  } else if (oyuncu == "Kağıt" && bilgisayar == "Kağıt") {
     console.log("Beraberlik!");
-  } else if (oyuncu == 'Makas' && bilgisayar == 'Makas') {
+  } else if (oyuncu == "Makas" && bilgisayar == "Makas") {
     console.log("Beraberlik");
-  } else if (oyuncu == 'Kağıt' && bilgisayar == 'Makas') {
-    console.log("Kaybettin");
-  } else if (oyuncu == 'Kağıt' && bilgisayar == 'Taş') {
+  } else if (oyuncu == "Kağıt" && bilgisayar == "Makas") {
+    console.log("Kaybettin!");
+  } else if (oyuncu == "Kağıt" && bilgisayar == "Taş") {
     console.log("Kazandın!");
-  } else if (oyuncu == 'Makas' && bilgisayar == 'Taş') {
+  } else if (oyuncu == "Makas" && bilgisayar == "Taş") {
     console.log("Kaybettin!");
-  } else if (oyuncu == 'Taş' && bilgisayar == 'Kağıt') {
+  } else if (oyuncu == "Taş" && bilgisayar == "Kağıt") {
     console.log("Kaybettin!");
-  } else if (oyuncu == 'Makas' && bilgisayar == 'Kağıt') {
+  } else if (oyuncu == "Makas" && bilgisayar == "Kağıt") {
     console.log("Kazandın!");
   } else {
     console.log("Kazandın!");
-
-
   }
 }
 
 function bilgisayarınSecimi(a) {
   if (0 <= a && a <= 0.33) {
-    secim = 'Taş';
+    secim = "Taş";
   } else if (0.34 <= a && a <= 0.66) {
-    secim = 'Makas';
+    secim = "Makas";
   } else {
-    secim = 'Kağıt';
+    secim = "Kağıt";
   }
   return secim;
 }
 let a = Math.random();
-let player = 'Taş';
+let player = "Kağıt";
 console.log(`Oyuncu seçimi ${player}`);
 console.log(`Bilgisayar seçimi :${bilgisayarınSecimi(a)}`);
 
-
 oyun(player, bilgisayarınSecimi(a));
 
-// 4a 
+// 4a
 function milDonusturucu(km) {
   let mil = km * 0.62137;
   return mil.toFixed(2);
@@ -111,18 +100,17 @@ console.log(feetDonusturucu(100));
 //5
 
 function cocukSarkisi(maymunSayısı) {
-
   for (let i = maymunSayısı; i > 0; i--) {
-    console.log(`${i} küçük maymun yatakta zıplamış, biri düşüp başını çarpmış, Anne doktoru aramış, Doktor çok kızmış: Bir daha yatakta zıplamak yok!`);
+    console.log(
+      `${i} küçük maymun yatakta zıplamış, biri düşüp başını çarpmış, Anne doktoru aramış, Doktor çok kızmış: Bir daha yatakta zıplamak yok!`
+    );
   }
-
 }
-cocukSarkisi(5)
+cocukSarkisi(5);
 
 //6
 
 function notHesapla(not) {
-
   if (not <= 100 && not >= 90) {
     console.log("A Aldın");
   } else if (not <= 89 && not >= 80) {
@@ -136,4 +124,4 @@ function notHesapla(not) {
   }
 }
 
-notHesapla(71);
+notHesapla(90);
